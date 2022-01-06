5ebb1a61a28c07bb4c840bd5c18317bccfd8445c
Olivier02500 <olivier.peuchet@hotmail.com>
2022-01-06 15:34:31 +0100

    make exo 132

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<?php
+
+$text = "Du texte est stocké";
+
+echo $text . '<br>';
+
+$y = 30 ;
+
+echo   'y vos ' . $y . '<br>';
+
+$m = 5;
+$n = 7;
+
+echo ' m + n =' . $m += $n;
+
+
+?>
+
+</body>
+</html>
